9ecd2d9536380c671589a74d2f719954fb1ab58a
Eralp Çelebi <eralp_celebi@hotmail.com>
2017-08-12 17:50:21 +0300

    Bellek alanından kazanmak amacı ile EGER döngüsü değiştirildi.

--- Evreka/parser.pyx
+++ Evreka/parser.pyx
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 symbols = {}
 
 
@@ -29,46 +30,7 @@ def parser(toks):
                     if toks[i + 1] == toks[i + 3]:
                         i += 5
                         while toks != 'YAP':
-                            if toks[i] == 'YAZDIR':
-                                data = toks[i + 1]
-                                if data[0] == '"':
-                                    data = data.replace('"', '')
-                                elif data[0:4] == 'SAYI':
-                                    data = data[5:]
-                                elif data[0:4] == 'OPER':
-                                    data = data[5:]
-                                    data = eval(data)
-                                elif data[0:3] == 'DEG':
-                                    #print(symbols)
-                                    data = alDEGISKEN(data[4:], symbols)
-                                print data
-                                i += 2
-                            elif toks[i][0:4] == 'DEG:' and toks[i + 1] == 'ESITTIR':
-                                if toks[i + 2][0] == '"':
-                                    data = toks[i + 2].replace('"', '')
-                                    YERLESTIR(toks[i], data)
-                                elif toks[i + 2][0:4] == 'SAYI':
-                                    data = toks[i + 2][5:]
-                                    YERLESTIR(toks[i], data)
-                                elif toks[i + 2][0:4] == 'OPER':
-                                    data = toks[i + 2][5:]
-                                    data = eval(data)
-                                    YERLESTIR(toks[i], data)
-                                elif toks[i + 2][0:4] == 'DEG:':
-                                    YERLESTIR(toks[i], alDEGISKEN(toks[i + 2][4:], symbols))
-                                elif toks[i + 2] == 'GIRDI':
-                                    data = input(toks[i + 3].replace('"', ''))
-                                    YERLESTIR(toks[i], data)
-                                i += 3
-                            elif toks[i] == "CIK":
-                                exit()
-                            else:
-                                i += 1
-
-                    else:
-                        i = 0
-                        while toks[i] != 'YAP':
-                            i += 1
+                            parser(toks)
 
             if toks[i] == 'YAZDIR':
                 data = toks[i + 1]
